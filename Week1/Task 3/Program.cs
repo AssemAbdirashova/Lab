46c254af86262a139311759a7aa85f7e35e99e9c
Assem <asemsuper2001@mail.ru>
2019-02-08 12:57:25 +0600

    'upd'

---
+++ Week1/Task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+            int n = int.Parse(line1);
+            string[] parts = line2.Split(new char[] { ' ', ',', '$' });
+            for(int i=0; i<n;i++)
+            {
+                Console.Write(parts[i] + " ");
+                Console.Write(parts[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
